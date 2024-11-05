f0234fb6cb06e036604c0a18d1cbea25bbe23c87
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-11-04 16:32:26 -0800

    respond to "PING" commands

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
+const string pong = "+PONG\r\n";
+
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var client = server.AcceptSocket(); // wait for client
+
+await client.SendAsync(Encoding.UTF8.GetBytes(pong), SocketFlags.None);
